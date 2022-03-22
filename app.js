b2548cc92abd598b094b9754db9a814922dc38a8
dmitri <dmitrisidorov@hotmail.com>
2022-03-22 15:11:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+var items = []
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res){
+
+    var today = new Date()
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    var day = today.toLocaleDateString("es-EU", options)
+
+    res.render("list", {kindOfday: day, newListItems: items})
+})
+
+app.post("/", function(req, res){
+    items.push(req.body.newItem)
+
+    res.redirect("/")
+})
+
+app.listen(3000, function(){
+    console.log("server started on port 3000")
+})
